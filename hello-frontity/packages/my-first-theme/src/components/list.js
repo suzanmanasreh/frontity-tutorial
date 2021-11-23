2a85673d205177c0a9cce9ce55b146e4b00d961b
Suzan M <suzanmanasreh@gmail.com>
2021-11-23 17:30:42 -0500

    display list of posts

---
+++ hello-frontity/packages/my-first-theme/src/components/list.js
@@ -0,0 +1,23 @@
+import React from "react";
+import { connect } from "frontity";
+import Link from "@frontity/components/link";
+
+const List = ({ state }) => {
+  const data = state.source.get(state.router.link);
+
+  return (
+    <div>
+      {data.items.map((item) => {
+        const post = state.source[item.type][item.id];
+        return (
+          <Link key={item.id} link={post.link}>
+            {post.title.rendered}
+            <br />
+          </Link>
+        );
+      })}
+    </div>
+  );
+};
+
+export default connect(List);
