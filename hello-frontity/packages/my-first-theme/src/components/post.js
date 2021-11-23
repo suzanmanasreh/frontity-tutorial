3ab83a0488145d14296b76e4116c6bb06160bf33
Suzan M <suzanmanasreh@gmail.com>
2021-11-23 18:49:54 -0500

    display post content

---
+++ hello-frontity/packages/my-first-theme/src/components/post.js
@@ -0,0 +1,17 @@
+import React from "react";
+import { connect } from "frontity";
+
+const Post = ({ state }) => {
+  const data = state.source.get(state.router.link);
+  const post = state.source[data.type][data.id];
+
+  return (
+    <div>
+      {console.log(post.content.rendered)}
+      <h2>{post.title.rendered}</h2>
+      <div dangerouslySetInnerHTML={{ __html: post.content.rendered }} />
+    </div>
+  );
+};
+
+export default connect(Post);
