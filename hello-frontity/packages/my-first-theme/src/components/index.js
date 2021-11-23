8fd16c8b8f2912203577bfa10d423f51f1e4afd5
Suzan M <suzanmanasreh@gmail.com>
2021-11-23 16:52:52 -0500

    understand frontity state

--- hello-frontity/packages/my-first-theme/src/components/index.js
+++ hello-frontity/packages/my-first-theme/src/components/index.js
@@ -1,8 +1,10 @@
 import React from "react";
 import { connect } from "frontity";
 import Link from "@frontity/components/link";
+import Switch from "@frontity/components/switch";
 
 const Root = ({ state }) => {
+  const data = state.source.get(state.router.link);
   return (
     <>
       <h1>Frontity Workshop</h1>
@@ -14,6 +16,14 @@ const Root = ({ state }) => {
         <br />
         <Link link="/about-us">About Us</Link>
       </nav>
+      <hr />
+      <main>
+        <Switch>
+          <div when={data.isArchive}>This is a list</div>
+          <div when={data.isPost}>This is a post</div>
+          <div when={data.isPage}>This is a page</div>
+        </Switch>
+      </main>
     </>
   );
 };
