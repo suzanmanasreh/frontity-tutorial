c2a71ce346b1473e0121f9f5e5070ae0b020bcbe
Suzan M <suzanmanasreh@gmail.com>
2021-11-23 16:03:22 -0500

    add menu

--- hello-frontity/packages/my-first-theme/src/components/index.js
+++ hello-frontity/packages/my-first-theme/src/components/index.js
@@ -1,11 +1,19 @@
 import React from "react";
 import { connect } from "frontity";
+import Link from "@frontity/components/link";
 
 const Root = ({ state }) => {
   return (
     <>
       <h1>Frontity Workshop</h1>
       <p>Current URL: {state.router.link}</p>
+      <nav>
+        <Link link="/">Home</Link>
+        <br />
+        <Link link="/page/2">More posts</Link>
+        <br />
+        <Link link="/about-us">About Us</Link>
+      </nav>
     </>
   );
 };
