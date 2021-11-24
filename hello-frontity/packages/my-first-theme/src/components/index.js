144608e6d38c9cdaa9e498e9c877e837726688d2
Suzan M <suzanmanasreh@gmail.com>
2021-11-23 21:07:18 -0500

    styling components

--- hello-frontity/packages/my-first-theme/src/components/index.js
+++ hello-frontity/packages/my-first-theme/src/components/index.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { connect } from "frontity";
+import { connect, Global, css, styled } from "frontity";
 import Link from "@frontity/components/link";
 import Switch from "@frontity/components/switch";
 import List from "./list";
@@ -10,25 +10,80 @@ const Root = ({ state }) => {
   const data = state.source.get(state.router.link);
   return (
     <>
-      <h1>Frontity Workshop</h1>
-      <p>Current URL: {state.router.link}</p>
-      <nav>
-        <Link link="/">Home</Link>
-        <br />
-        <Link link="/page/2">More posts</Link>
-        <br />
-        <Link link="/about-us">About Us</Link>
-      </nav>
-      <hr />
-      <main>
+      <Global
+        styles={css`
+          * {
+            margin: 0;
+            padding: 0;
+            box-sizing: border-box;
+          }
+          html {
+            font-family: system-ui, Verdana, Arial, sans-serif;
+          }
+        `}
+      />
+      <Header>
+        <HeaderContent>
+          <h1>Frontity Workshop</h1>
+          <p>Current URL: {state.router.link}</p>
+          <nav>
+            <Link link="/">Home</Link>
+            <br />
+            <Link link="/page/2">More posts</Link>
+            <br />
+            <Link link="/about-us">About Us</Link>
+          </nav>
+        </HeaderContent>
+      </Header>
+      <Main>
         <Switch>
           <List when={data.isArchive} />
           <Post when={data.isPost} />
           <Page when={data.isPage} />
         </Switch>
-      </main>
+      </Main>
     </>
   );
 };
 
 export default connect(Root);
+
+const Header = styled.header`
+  background-color: #e5edee;
+  border-width: 0 0 8px 0;
+  border-style: solid;
+  border-color: maroon;
+  line-height: 1.3;
+
+  h1 {
+    color: #4a4a4a;
+    line-height: 1.6;
+  }
+`;
+
+const HeaderContent = styled.div`
+  max-width: 800px;
+  padding: 2em 1em;
+  margin: auto;
+`;
+
+const Main = styled.main`
+  max-width: 800px;
+  padding: 1em;
+  margin: auto;
+  img {
+    max-width: 100%;
+  }
+  h2 {
+    margin: 0.5em 0;
+  }
+  p {
+    line-height: 1.25em;
+    margin-bottom: 0.75em;
+  }
+  figcaption {
+    color: #828282;
+    font-size: 0.8em;
+    margin-bottom: 1em;
+  }
+`;
