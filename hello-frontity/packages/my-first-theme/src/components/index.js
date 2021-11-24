e2cdb4e50b2db4c11455676c8ba64eedc18b92e8
Suzan M <suzanmanasreh@gmail.com>
2021-11-23 21:22:49 -0500

    styling links

--- hello-frontity/packages/my-first-theme/src/components/index.js
+++ hello-frontity/packages/my-first-theme/src/components/index.js
@@ -26,13 +26,13 @@ const Root = ({ state }) => {
         <HeaderContent>
           <h1>Frontity Workshop</h1>
           <p>Current URL: {state.router.link}</p>
-          <nav>
+          <Menu>
             <Link link="/">Home</Link>
             <br />
             <Link link="/page/2">More posts</Link>
             <br />
             <Link link="/about-us">About Us</Link>
-          </nav>
+          </Menu>
         </HeaderContent>
       </Header>
       <Main>
@@ -85,5 +85,16 @@ const Main = styled.main`
     color: #828282;
     font-size: 0.8em;
     margin-bottom: 1em;
+  }
+`;
+
+const Menu = styled.nav`
+  display: flex;
+  flex-direction: row;
+  margin-top: 1em;
+  & > a {
+    margin-right: 1em;
+    color: steelblue;
+    text-decoration: none;
   }
 `;
